1bf076338fc26d0eb7ceeba5c9eb8510920fc3af
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2014-05-15 09:47:03 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+var tableize = require('tableize'),
+  regret = require('regret');
+
+regret.add('date',
+  /\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d-[0-5]\d/,
+  '2012-02-14T20:58:35-04:00');
+
+var infer = module.exports = function(sample, opts) {
+  opts = opts || {};
+  var obj = tableize(sample),
+    geo = [];
+
+  return Object.keys(obj).reduce(function(schema, key){
+    var val = obj[key];
+    var type = typeof val;
+
+    if (type){
+      schema[key] = type;
+    }
+    if(/_url$/.test(key)){
+      schema[key] = 'url';
+    }
+    else if(/code$/.test(key)){
+      schema[key] = 'enum';
+    }
+    else if(key === 'description' || key === 'summary'){
+      schema[key] = 'text';
+    }
+    else if(/_id$/.test(key)){
+      schema[key] += ' id';
+    }
+    else if(regret('date', sample[key])){
+      schema[key] = 'date';
+    }
+    else if(key === 'lat'){
+      schema[key] = 'geo lat';
+    }
+    else if(key === 'long'){
+      schema[key] = 'geo long';
+    }
+    return schema;
+  }, {});
+};
---
+++ test.js
@@ -0,0 +1,48 @@
+var infer = require('./');
+
+describe('infer', function(){
+  it('should work', function(){
+    var expect = {
+        _id: 'string id',
+        service_request_id: 'number id',
+        status: 'string',
+        status_notes: 'string',
+        service_name: 'string',
+        service_code: 'enum',
+        description: 'text',
+        agency_responsible: 'string',
+        service_notice: 'object',
+        requested_datetime: 'date',
+        updated_datetime: 'date',
+        expected_datetime: 'date',
+        address: 'string',
+        address_id: 'number id',
+        zipcode: 'enum',
+        long: 'geo long',
+        lat: 'geo lat',
+        media_url: 'url'
+      },
+      res = infer({
+        "_id": "536c02d34efcef0c2f2439b9",
+        "service_request_id": 101001366957,
+        "status": "closed",
+        "status_notes": "Completed - The request has been concluded.",
+        "service_name": "Road - Pot hole",
+        "service_code": "CSROWR-12",
+        "description": null,
+        "agency_responsible": "311 Toronto",
+        "service_notice": null,
+        "requested_datetime": "2012-02-14T20:58:35-04:00",
+        "updated_datetime": "2012-02-17T07:56:00-04:00",
+        "expected_datetime": "2012-05-14T20:59:00-04:00",
+        "address": "46 LAMBTON AVE",
+        "address_id": 9544823,
+        "zipcode": null,
+        "long": -79.48746252100001,
+        "lat": 43.683629226,
+        "media_url": null
+      });
+
+      assert.deepEqual(expect, res);
+  });
+});
